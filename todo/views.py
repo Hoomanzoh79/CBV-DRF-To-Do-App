3f9a0ca9af2ec3a4ea5f94d95ff20d1dc2296050
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-06 22:53:47 +0330

    add template for login

--- todo/views.py
+++ todo/views.py
@@ -2,7 +2,7 @@ from django.views import generic
 from .models import Task
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-class TaskListView(LoginRequiredMixin,generic.ListView):
+class TaskListView(generic.ListView):
     queryset = Task.objects.filter(is_done=False)
     template_name = 'todo/task_list.html'
     context_object_name = 'tasks'
