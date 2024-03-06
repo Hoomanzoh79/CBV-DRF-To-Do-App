65f86402226afa5d13ed0fa7a1d804ce37e8be05
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-06 22:25:47 +0330

    create model Task

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Task
 
-# Register your models here.
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    model = Task
+    list_display = ['title','is_done','author']
--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Task(models.Model):
+    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    title = models.CharField(max_length=250)
+    is_done = models.BooleanField(default=False)
+    
+    datetime_created = models.DateTimeField(auto_now_add=True)
+    datetime_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
