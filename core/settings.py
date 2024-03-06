3f8476c0617b70874cdd4401e4221fb73ed27cae
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-06 22:18:22 +0330

    add 2 new apps

--- core/settings.py
+++ core/settings.py
@@ -40,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # my apps
+    'todo',
+    'accounts',
 ]
 
 MIDDLEWARE = [
