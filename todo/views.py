331692813672ff7b9340870232f5a349591020f6
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-07 09:56:33 +0330

    add new mixin for TaskListView and TaskDetailView

--- todo/views.py
+++ todo/views.py
@@ -4,14 +4,14 @@ from django.views import generic
 from .models import Task
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-class TaskListView(generic.ListView):
+class TaskListView(LoginRequiredMixin,generic.ListView):
     template_name = 'todo/task_list.html'
     context_object_name = 'tasks'
 
     def get_queryset(self):
         return Task.objects.filter(author=self.request.user)
 
-class TaskDetailView(generic.DetailView):
+class TaskDetailView(LoginRequiredMixin,generic.DetailView):
     model = Task
     template_name = 'todo/task_detail.html'
     context_object_name = 'task'
