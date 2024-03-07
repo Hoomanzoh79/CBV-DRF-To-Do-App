de81fa2b0542607ca245d337815638f3af0bef6b
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-08 00:35:26 +0330

    add signup

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
+from .views import SignUpView
 
 app_name = "accounts"
 
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
+    path("signup/",SignUpView.as_view(),name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views import generic
+from django.urls import reverse_lazy
+from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
+from accounts.models import User
+
+
+class SignUpView(generic.CreateView):
+    form_class = UserCreationForm
+    success_url = reverse_lazy("accounts:login")
+    template_name = "registration/signup.html"
