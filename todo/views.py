715e21754b76515fafd05145a81f56e9352f2053
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-07 10:19:22 +0330

    change queryset for TaskDetailView

--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,3 @@
-from typing import Any
-from django.db.models.query import QuerySet
 from django.views import generic
 from .models import Task
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -12,6 +10,8 @@ class TaskListView(LoginRequiredMixin,generic.ListView):
         return Task.objects.filter(author=self.request.user)
 
 class TaskDetailView(LoginRequiredMixin,generic.DetailView):
-    model = Task
     template_name = 'todo/task_detail.html'
     context_object_name = 'task'
+    
+    def get_queryset(self):
+        return Task.objects.filter(author=self.request.user)
