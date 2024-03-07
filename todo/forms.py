12ea5aba77bc93979d4ab480baf937606449a749
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-07 11:08:29 +0330

    add TaskUpdateForm

---
+++ todo/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from .models import Task
+
+
+class TaskUpdateForm(forms.ModelForm):
+    title = forms.CharField()
+    
+    class Meta:
+        model = Task
+        fields = ("title",)
