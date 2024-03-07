ab5acf514b8967dce9f958ecdb0411985cd313ae
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-07 09:46:58 +0330

    add custom user

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,61 @@
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 from django.contrib import admin
 
-# Register your models here.
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    # form
+    # add_form
+    model = User
+    list_display = (
+        "email",
+        "is_superuser",
+        "is_active",
+    )
+    list_filter = (
+        "email",
+        "is_superuser",
+        "is_active",
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
+    fieldsets = (
+        ("Authentication", {"fields": ("email", "password")}),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+                )
+            },
+        ),
+        (
+            "Group Permissions",
+            {
+                "fields": (
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        ("Important Data", {"fields": ("last_login",)}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+        
+                ),
+            },
+        ),
+    )
