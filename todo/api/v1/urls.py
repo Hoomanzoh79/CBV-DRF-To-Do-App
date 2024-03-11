8fcecaa4a90fa7518162ebebb9633e8c471fa4c4
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-11 22:55:43 +0330

    add rouer for TaskViewSet

--- todo/api/v1/urls.py
+++ todo/api/v1/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
-from . import views
+from .views import TaskViewSet
+from rest_framework.routers import DefaultRouter
 
 app_name = "api-v1"
 
-urlpatterns = [
-    
-]
+router = DefaultRouter()
+router.register("task", TaskViewSet, basename="task")
+
+urlpatterns = router.urls
