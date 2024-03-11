a3354803a76cc7431556e79c51f3a80a154db98e
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-11 14:22:14 +0330

    add rest framework urls

--- core/urls.py
+++ core/urls.py
@@ -20,4 +20,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('accounts.urls')),
     path('todo/',include('todo.urls')),
+    # rest framework
+    path('api-auth/', include('rest_framework.urls')),
 ]
