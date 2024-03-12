5732b3fee2c23adf1a61e5f0649df150ab023e23
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-12 11:40:41 +0330

    change author to Profile instance

--- todo/models.py
+++ todo/models.py
@@ -1,8 +1,8 @@
 from django.db import models
-from django.contrib.auth import get_user_model
+from accounts.models import Profile
 
 class Task(models.Model):
-    author = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
+    author = models.ForeignKey(Profile, on_delete=models.CASCADE)
     title = models.CharField(max_length=250)
     is_done = models.BooleanField(default=False)
     
