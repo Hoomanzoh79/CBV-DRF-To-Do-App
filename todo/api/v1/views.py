492a492806e047c835255139d7e46238fd18a6ca
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-12 11:17:22 +0330

    add filter backends and filterset fields

--- todo/api/v1/views.py
+++ todo/api/v1/views.py
@@ -4,14 +4,13 @@ from rest_framework import viewsets
 # from .permissions import IsOwnerPermission
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
-# from .paginations import DefaultPagination
 
 
 class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
     queryset = Task.objects.filter(is_done=False)
 
-    # filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
-    # filterset_fields = {"author": ["exact", "in"]}
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    filterset_fields = {"author": ["exact", "in"]}
     search_fields = ["title"]
     ordering_fields = ["datetime_created"]
