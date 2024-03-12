0864d063949376e923d3e8c62c48fe161f4d0ab6
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-12 13:05:26 +0330

    change get_queryset method

--- todo/api/v1/views.py
+++ todo/api/v1/views.py
@@ -1,20 +1,28 @@
 from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated
 
 from .serializers import TaskSerializer
 from todo.models import Task
 from .paginations import DefaultPagination
 from .permissions import IsOwnerPermission
+from accounts.models import Profile
 
 class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
-    queryset = Task.objects.all()
 
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = {"author": ["exact", "in"]}
     search_fields = ["title"]
     ordering_fields = ["datetime_created"]
     pagination_class = DefaultPagination
-    permission_classes = [IsAuthenticatedOrReadOnly,IsOwnerPermission]
+    permission_classes = [IsAuthenticated,IsOwnerPermission]
+
+    def get_queryset(self):
+        user = self.request.user
+        queryset = Task.objects.all()
+        if user.is_superuser & user.is_staff:
+            return queryset
+        
+        return queryset.filter(author=Profile.objects.get(user_id=user.id))
