0077e0bc4312478eae6ae9197f45d43e303f7129
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-12 12:43:10 +0330

    add DefaultPagination

--- todo/api/v1/views.py
+++ todo/api/v1/views.py
@@ -1,10 +1,10 @@
-from .serializers import TaskSerializer
-from todo.models import Task
 from rest_framework import viewsets
-# from .permissions import IsOwnerPermission
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 
+from .serializers import TaskSerializer
+from todo.models import Task
+from .paginations import DefaultPagination
 
 class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
@@ -14,3 +14,4 @@ class TaskViewSet(viewsets.ModelViewSet):
     filterset_fields = {"author": ["exact", "in"]}
     search_fields = ["title"]
     ordering_fields = ["datetime_created"]
+    pagination_class = DefaultPagination
