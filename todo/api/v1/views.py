16da64677d24968732e5014441479ba8415bb974
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-12 12:47:33 +0330

    add permission classes

--- todo/api/v1/views.py
+++ todo/api/v1/views.py
@@ -1,10 +1,12 @@
 from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .serializers import TaskSerializer
 from todo.models import Task
 from .paginations import DefaultPagination
+from .permissions import IsOwnerPermission
 
 class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
@@ -15,3 +17,4 @@ class TaskViewSet(viewsets.ModelViewSet):
     search_fields = ["title"]
     ordering_fields = ["datetime_created"]
     pagination_class = DefaultPagination
+    permission_classes = [IsAuthenticatedOrReadOnly,IsOwnerPermission]
