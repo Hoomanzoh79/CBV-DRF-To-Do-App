e04ec19c639ce3d73a75bbd87d9d35c4fb6c63b5
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-12 11:41:09 +0330

    change author to profile instance

--- todo/views.py
+++ todo/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import redirect
 
 from .forms import TaskUpdateForm
 from .models import Task
+from accounts.models import Profile
 
 class HomePageView(generic.TemplateView):
     template_name = 'home.html'
@@ -15,14 +16,14 @@ class TaskListView(LoginRequiredMixin,generic.ListView):
     paginate_by = 2
 
     def get_queryset(self):
-        return Task.objects.filter(author=self.request.user).order_by('-datetime_created')
+        return Task.objects.filter(author=Profile.objects.get(user_id=self.request.user.id)).order_by('-datetime_created')
 
 class TaskDetailView(LoginRequiredMixin,generic.DetailView):
     template_name = 'todo/task_detail.html'
     context_object_name = 'task'
     
     def get_queryset(self):
-        return Task.objects.filter(author=self.request.user)
+        return Task.objects.filter(author=Profile.objects.get(user_id=self.request.user.id))
 
 class TaskCreateView(LoginRequiredMixin,generic.CreateView):
     model = Task
@@ -31,7 +32,7 @@ class TaskCreateView(LoginRequiredMixin,generic.CreateView):
     success_url = reverse_lazy("todo:task-list")
     
     def form_valid(self, form):
-        form.instance.author = self.request.user
+        form.instance.author = Profile.objects.get(user_id=self.request.user.id)
         return super(TaskCreateView,self).form_valid(form)
 
 class TaskUpdateView(LoginRequiredMixin,generic.UpdateView):
@@ -40,14 +41,14 @@ class TaskUpdateView(LoginRequiredMixin,generic.UpdateView):
     template_name = "todo/task_update.html"
 
     def get_queryset(self):
-        return Task.objects.filter(author=self.request.user)
+        return Task.objects.filter(author=Profile.objects.get(user_id=self.request.user.id))
 
 class TaskDeleteView(LoginRequiredMixin,generic.DeleteView):
     success_url = reverse_lazy("todo:task-list")
     template_name = "todo/task_delete.html"
     
     def get_queryset(self):
-        return Task.objects.filter(author=self.request.user)
+        return Task.objects.filter(author=Profile.objects.get(user_id=self.request.user.id))
 
 class TaskDoneView(LoginRequiredMixin,generic.View):    
     model = Task
