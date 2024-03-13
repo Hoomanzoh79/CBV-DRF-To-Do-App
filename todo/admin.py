d30211fca74d26a937fe56a27ab4472d2f6473b3
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-13 09:52:07 +0330

    add status

--- todo/admin.py
+++ todo/admin.py
@@ -4,4 +4,4 @@ from .models import Task
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     model = Task
-    list_display = ['title','is_done','author']
+    list_display = ['title','is_done','status','author']
