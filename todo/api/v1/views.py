3fbf5a3a0bbf7dd8abd1c7ce72dabe9c9285b504
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-13 09:37:41 +0330

    change permission classes and queryset

--- todo/api/v1/views.py
+++ todo/api/v1/views.py
@@ -1,7 +1,7 @@
 from rest_framework import viewsets
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from .serializers import TaskSerializer
 from todo.models import Task
@@ -11,18 +11,11 @@ from accounts.models import Profile
 
 class TaskViewSet(viewsets.ModelViewSet):
     serializer_class = TaskSerializer
+    queryset = Task.objects.all()
 
     filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
     filterset_fields = {"author": ["exact", "in"]}
     search_fields = ["title"]
     ordering_fields = ["datetime_created"]
     pagination_class = DefaultPagination
-    permission_classes = [IsAuthenticated,IsOwnerPermission]
-
-    def get_queryset(self):
-        user = self.request.user
-        queryset = Task.objects.all()
-        if user.is_superuser & user.is_staff:
-            return queryset
-        
-        return queryset.filter(author=Profile.objects.get(user_id=user.id))
+    permission_classes = [IsAuthenticatedOrReadOnly,IsOwnerPermission]
