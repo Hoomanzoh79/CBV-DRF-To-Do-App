392b6bbf2bfa5eb0bd33e1b2b3d57892ba8afb3b
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-13 09:48:41 +0330

    add field status to task model

--- todo/models.py
+++ todo/models.py
@@ -5,6 +5,7 @@ class Task(models.Model):
     author = models.ForeignKey(Profile, on_delete=models.CASCADE)
     title = models.CharField(max_length=250)
     is_done = models.BooleanField(default=False)
+    status = models.BooleanField(default=True)
     
     datetime_created = models.DateTimeField(auto_now_add=True)
     datetime_modified = models.DateTimeField(auto_now=True)
