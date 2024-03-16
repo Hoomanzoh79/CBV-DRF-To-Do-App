79b072926e2e339fe75f0ed2a42a2a5acc9beda0
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-16 14:22:24 +0330

    add schema view and paths for swagger and redoc

--- core/urls.py
+++ core/urls.py
@@ -14,7 +14,25 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from rest_framework.documentation import include_docs_urls
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="CBV-DRF-To-Do-App",
+        default_version="v1",
+        description="This is a test for CBV-DRF-To-Do-App project",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="hoomanemi1999@gmail.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -22,4 +40,22 @@ urlpatterns = [
     path('todo/',include('todo.urls')),
     # rest framework
     path('api-auth/', include('rest_framework.urls')),
+
+    # Api documentation
+    path("api-docs/", include_docs_urls(title="api sample")),
+    path(
+        "swagger<format>/",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path(
+        "redoc/",
+        schema_view.with_ui("redoc", cache_timeout=0),
+        name="schema-redoc",
+    ),
 ]
