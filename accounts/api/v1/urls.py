1f06f1049acd369e351f05e9aca357b82f15f919
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-18 13:08:16 +0330

    add paths for jwt login

--- accounts/api/v1/urls.py
+++ accounts/api/v1/urls.py
@@ -1,9 +1,21 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
 
 urlpatterns = [
     # token login and logout
     path("token/login/", views.CustomAuthToken.as_view(), name="token-login"),
     path("token/logout/", views.DiscardAuthToken.as_view(), name="token-logout"),
+    # jwt login 
+    path(
+        "jwt/create/",
+        views.CustomTokenObtainPairView.as_view(),
+        name="jwt-create",
+    ),
+    path("jwt/refresh/", TokenRefreshView.as_view(), name="jwt-refresh"),
+    path("jwt/verify/", TokenVerifyView.as_view(), name="jwt-verify"),
 ]
