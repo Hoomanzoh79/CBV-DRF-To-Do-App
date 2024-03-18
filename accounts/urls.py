47d722cb17bb4f7eec7dfc319f0feb52bce54a2b
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-18 12:39:11 +0330

    add path for api

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,6 @@ app_name = "accounts"
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
     path("signup/",SignUpView.as_view(),name='signup'),
+    # API 
+    path("api/v1/", include("accounts.api.v1.urls")),
 ]
