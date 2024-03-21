9d60c995e940a701ddc4deef68116a7d52d3be00
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-03-21 18:33:50 +0330

    add new urls for drf spectacular

--- core/urls.py
+++ core/urls.py
@@ -19,20 +19,8 @@ from rest_framework.documentation import include_docs_urls
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="CBV-DRF-To-Do-App",
-        default_version="v1",
-        description="This is a test for CBV-DRF-To-Do-App project",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="hoomanemi1999@gmail.com"),
-        license=openapi.License(name="MIT License"),
-    ),
-    public=True,
-    permission_classes=(permissions.AllowAny,),
-)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -41,21 +29,9 @@ urlpatterns = [
     # rest framework
     path('api-auth/', include('rest_framework.urls')),
 
-    # Api documentation
-    path("api-docs/", include_docs_urls(title="api sample")),
-    path(
-        "swagger/output.json",
-        schema_view.without_ui(cache_timeout=0),
-        name="schema-json",
-    ),
-    path(
-        "swagger/",
-        schema_view.with_ui("swagger", cache_timeout=0),
-        name="schema-swagger-ui",
-    ),
-    path(
-        "redoc/",
-        schema_view.with_ui("redoc", cache_timeout=0),
-        name="schema-redoc",
-    ),
+    # API SCHEMA
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Optional UI:
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
