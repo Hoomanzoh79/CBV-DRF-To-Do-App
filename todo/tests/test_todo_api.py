e15c38bb703ddd5d769e0fa459663d43eecb0958
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-04-03 13:06:20 +0330

    reformat files

--- todo/tests/test_todo_api.py
+++ todo/tests/test_todo_api.py
@@ -1,7 +1,7 @@
 import pytest
 from rest_framework.test import APIClient
 from django.urls import reverse
-from accounts.models import User,Profile
+from accounts.models import User, Profile
 from todo.models import Task
 
 
@@ -16,16 +16,26 @@ def common_user():
     user = User.objects.create_user(email="test@test.com", password="Aa@1234%")
     return user
 
+
 @pytest.fixture
 def profile(common_user):
-    profile = Profile.objects.create(user=common_user,first_name="test firstname",
-                                     last_name="test lastname",bio="test bio",)
+    profile = Profile.objects.create(
+        user=common_user,
+        first_name="test firstname",
+        last_name="test lastname",
+        bio="test bio",
+    )
     return profile
+
 
 @pytest.fixture
 def task_example(profile):
-    task = Task.objects.create(title="test task",status=True,author=profile,
-            is_done=False,)
+    task = Task.objects.create(
+        title="test task",
+        status=True,
+        author=profile,
+        is_done=False,
+    )
 
     return task
 
@@ -42,7 +52,7 @@ class TestTaskApi:
         data = {
             "title": "Go to the gym",
             "is_done": False,
-            "status":True,
+            "status": True,
         }
         response = api_client.post(url, data)
         assert response.status_code == 401
@@ -52,13 +62,13 @@ class TestTaskApi:
         data = {
             "title": "Go to the gym",
             "is_done": False,
-            "status":True,
+            "status": True,
         }
         user = common_user
         api_client.force_authenticate(user=user)
         response = api_client.post(url, data)
         assert response.status_code == 201
-    
+
     def test_post_task_invalid_data_status_400(self, api_client, common_user):
         url = reverse("todo:api-v1:task-list")
         invalid_data = {}
@@ -66,40 +76,46 @@ class TestTaskApi:
         api_client.force_authenticate(user=user)
         response = api_client.post(url, invalid_data)
         assert response.status_code == 400
-    
-    def test_delete_task_authorized_status_201(self, api_client, common_user,task_example):
-        url = reverse("todo:api-v1:task-detail",args=[task_example.id])
+
+    def test_delete_task_authorized_status_201(
+        self, api_client, common_user, task_example
+    ):
+        url = reverse("todo:api-v1:task-detail", args=[task_example.id])
         user = common_user
         api_client.force_authenticate(user=user)
         response = api_client.delete(url)
         assert response.status_code == 204
-    
-    def test_delete_task_unauthorized_status_401(self, api_client,task_example):
-        url = reverse("todo:api-v1:task-detail",args=[task_example.id])
+
+    def test_delete_task_unauthorized_status_401(self, api_client, task_example):
+        url = reverse("todo:api-v1:task-detail", args=[task_example.id])
         response = api_client.delete(url)
         assert response.status_code == 401
-    
-    def test_put_task_authorized_status_200(self, api_client, common_user,task_example):
-        url = reverse("todo:api-v1:task-detail",args=[task_example.id])
+
+    def test_put_task_authorized_status_200(
+        self, api_client, common_user, task_example
+    ):
+        url = reverse("todo:api-v1:task-detail", args=[task_example.id])
         user = common_user
         data = {
             "title": "Go to the gym",
             "is_done": False,
-            "status":True,
+            "status": True,
         }
         api_client.force_authenticate(user=user)
-        response = api_client.put(url,data)
+        response = api_client.put(url, data)
         assert response.status_code == 200
-    
-    def test_put_task_unauthorized_status_401(self, api_client,task_example):
-        url = reverse("todo:api-v1:task-detail",args=[task_example.id])
+
+    def test_put_task_unauthorized_status_401(self, api_client, task_example):
+        url = reverse("todo:api-v1:task-detail", args=[task_example.id])
         response = api_client.put(url)
         assert response.status_code == 401
-    
-    def test_put_task_invalid_data_status_400(self, api_client, common_user,task_example):
-        url = reverse("todo:api-v1:task-detail",args=[task_example.id])
+
+    def test_put_task_invalid_data_status_400(
+        self, api_client, common_user, task_example
+    ):
+        url = reverse("todo:api-v1:task-detail", args=[task_example.id])
         user = common_user
         invalid_data = {}
         api_client.force_authenticate(user=user)
-        response = api_client.put(url,invalid_data)
+        response = api_client.put(url, invalid_data)
         assert response.status_code == 400
