0419330dd544593bd7a486645d6e28920792f3ba
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-04-22 13:00:21 +0330

    add new task

--- todo/tasks.py
+++ todo/tasks.py
@@ -1,8 +1,15 @@
 from celery import shared_task
 from time import sleep
+from todo.models import Task
 
+
+# @shared_task
+# def sendEmail():
+#     sleep(3)
+#     print("tasks deleted")
 
 @shared_task
-def sendEmail():
-    sleep(3)
-    print("done sending email *celery task")
+def deleteTask():
+    sleep(5)
+    Task.objects.filter(is_done=True).delete()
+    print("done tasks deleted successfully")
