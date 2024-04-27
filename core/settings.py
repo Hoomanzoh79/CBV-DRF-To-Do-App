dd94aa188604a46740e6668aec48f86da806f365
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-04-27 11:50:33 +0330

    add caching configs

--- core/settings.py
+++ core/settings.py
@@ -173,3 +173,14 @@ SPECTACULAR_SETTINGS = {
 
 # celery configs
 CELERY_BROKER_URL = 'redis://redis:6379/1'
+
+# caching configs
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/2",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        },
+    }
+}
