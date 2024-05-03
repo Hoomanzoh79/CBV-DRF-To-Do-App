233aa9e6d62304ce1e9c407b0796f02dc293e382
Hooman Zohrevand <86908448+Hoomanzoh79@users.noreply.github.com>
2024-05-03 15:07:24 +0330

    add static and media

--- core/settings.py
+++ core/settings.py
@@ -131,8 +131,17 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "static"
+
+# Media files
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
+
+STATICFILES_DIRS = [
+    BASE_DIR / "staticfiles",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
